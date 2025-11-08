40faed55370f32e17065d92aa9f3b6d220c07dfd
[dyad] <penmadkotatasik@gmail.com>
2025-11-08 10:54:44 +0700

    [dyad] Menyesuaikan tata letak footer - wrote 1 file(s)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,10 @@ import { Link } from 'react-router-dom';
 export const Footer = () => {
   return (
     <footer className="p-4 text-center">
-      <div className="flex justify-center items-center space-x-4 text-sm text-gray-500 dark:text-gray-400">
+      <div className="flex flex-col justify-center items-center space-y-2 text-sm text-gray-500 dark:text-gray-400">
         <Link to="/about" className="hover:text-gray-700 dark:hover:text-gray-200">
           About
         </Link>
-        <span>|</span>
         <a
           href="https://www.dyad.sh/"
           target="_blank"
